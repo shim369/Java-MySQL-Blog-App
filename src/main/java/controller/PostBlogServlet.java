06e08810644a0b9945ecaebdf319ef22bed990aa
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-26 10:54:10 +0900

    update file name

--- src/main/java/controller/PostBlogServlet.java
+++ src/main/java/controller/PostBlogServlet.java
@@ -32,7 +32,7 @@ public class PostBlogServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/loginUserPage.jsp");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/loginUser.jsp");
 		dispatcher.forward(request, response);
 	}
 
